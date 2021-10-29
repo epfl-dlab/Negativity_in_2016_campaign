09b553b17646f4c751ff8996587a07c9c26a44a5
Jonathan Kuelz <jonathan.kuelz@tum.de>
2021-10-29 14:00:07 +0200

    column naming bug

--- analysis/aggregate.py
+++ analysis/aggregate.py
@@ -38,7 +38,7 @@ def _prep_people(df: DataFrame) -> DataFrame:
 
     return df \
         .withColumn('congress_member', (f.size('CIDs') > 0).cast('integer')) \
-        .select('qid', 'congress_member', 'gender', f.explode('parties').alias('tmp_party')) \
+        .select('qid', 'congress_member', 'genders', f.explode('parties').alias('tmp_party')) \
         .select('*', f.explode('genders').alias('tmp_gender')) \
         .select('qid', 'congress_member', __map_party('tmp_party').alias('party'), __map_gender('tmp_gender').alias('gender')) \
         .dropna(how='any', subset=['gender', 'party'])
