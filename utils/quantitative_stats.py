deec0a8265fb165ea4fdd7c4fe859190a8fd7cba
Jonathan Kuelz <jonathan.kuelz@tum.de>
2021-11-11 18:01:12 +0100

    overview stats

---
+++ utils/quantitative_stats.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+
+parser = ArgumentParser()
+parser.add_argument('--quotations', help='Path to quotations DF', required=True)
+parser.add_argument('--save', help='Path where to save Dataframe (csv)', required=True)
+
+
+def main():
+    args = parser.parse_args()
+    spark = SparkSession.builder.getOrCreate()
+    spark.sparkContext.setLogLevel('WARN')
+
+    df = spark.read.parquet(args.quotations)
+    stats = df \
+        .withColumn('numDomains', f.size('domains')) \
+        .groupby(['year', 'month']) \
+        .agg(f.countDistinct('qid').alias('num_speaker'),
+             f.countDistinct('quoteID').alias('num_quotes'),
+             f.sum('numDomains').alias('total_domains'),
+             ) \
+        .toPandas()
+
+    stats.to_csv(open(args.save, 'w'))
+
+
+if __name__ == '__main__':
+    main()
