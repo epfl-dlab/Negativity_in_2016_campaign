503f5f86576ab34cc8864c438c613754c2cd593b
Jonathan Kuelz <jonathan.kuelz@tum.de>
2021-12-16 18:22:08 +0100

    New SI in new order and improved captions

--- SI/sentiment_regex_evaluation.py
+++ SI/sentiment_regex_evaluation.py
@@ -9,11 +9,12 @@ from typing import Dict
 import warnings
 
 sys.path.append(str(Path(__file__).parent.parent))  # Only works when keeping the original repo structure
-from analysis.figures import CORE_FEATURES, NAMES, ONE_COL_FIGSIZE, FONTSIZE
+from analysis.RDD import NAMES
+from analysis.figures import CORE_FEATURES
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data', help='Pickle file made by the according analysis script', required=True)
+parser.add_argument('--data', help='Pickle file for word_by_word_sentiment made by the according analysis script', required=True)
 parser.add_argument('--img', help='Image Folder', required=True)
 parser.add_argument('--tables', help='Tables Folder', required=True)
 
@@ -74,24 +75,28 @@ def evaluate_sentiment_patterns(counts: Dict, wordMap: Dict, categoryMap: Dict, 
     TOP_N = 30
     for name, cnt in counts.items():
         #  Get the share of each word for each category
+        multi = pd.MultiIndex.from_product([sorted(set(categoryMap.values())), ['pattern', 'percentage']])
         tops = pd.DataFrame(index=[str(i+1) for i in range(TOP_N)] + ['Total nr Expressions', 'Total Matches'],
-                            columns=sorted(set(categoryMap.values())))
+                            columns=multi)
         tops.index.name = 'Top N words'
-        for cat in tops.columns:
+        for cat in sorted(set(categoryMap.values())):
             indices = np.asarray([idx for idx, ctgry in categoryMap.items() if ctgry == cat])
             matchingCounts = cnt[indices]
             matchingWords = [word for idx, word in wordMap.items() if idx in indices]
             sortIndices = np.argsort(matchingCounts)[::-1]  # Sorting large to small values
             totalMatches = sum(matchingCounts)
-            topsInfo = [f"{matchingWords[i]}: {100 * matchingCounts[i] / totalMatches:.2f}%" for i in sortIndices[:TOP_N]]
-            tops[cat] = topsInfo + [str(len(matchingWords)), str(int(totalMatches))]
+            topsRegex = [f"{matchingWords[i]}" for i in sortIndices[:TOP_N]]
+            topsPercentage = [f"{100 * matchingCounts[i] / totalMatches:.2f}%" for i in sortIndices[:TOP_N]]
+            tops[cat] = np.asarray(list(zip(topsRegex, topsPercentage)) + [[str(len(matchingWords)), ''], [str(int(totalMatches)), '']])
 
         tops = tops.applymap(_manual_escape)
         topTables[name] = tops
 
     numericIndices = [str(i+1) for i in range(TOP_N)]
-    diff = topTables['before'].loc[numericIndices].applymap(_extract_numeric) - topTables['after'].loc[numericIndices].applymap(_extract_numeric)
+    def _numCols(df): return df[df.columns[1::2]]
+    diff = _numCols(topTables['before']).loc[numericIndices].applymap(_extract_numeric) - _numCols(topTables['after']).loc[numericIndices].applymap(_extract_numeric)
     bold = diff.applymap(abs) > 1
+    bold.columns = bold.columns.droplevel(1)  # Removing the "percentage", making it valid for both cols again
 
     def _maybeBold(s: str, isBold: bool):
         if isBold:
@@ -100,26 +105,29 @@ def evaluate_sentiment_patterns(counts: Dict, wordMap: Dict, categoryMap: Dict, 
 
     for name in counts:
         for idx in numericIndices:
-            for cat in topTables[name].columns:
-                topTables[name].at[idx, cat] = _maybeBold(topTables[name].at[idx, cat], bold.at[idx, cat])
+            for (cat, subCol) in topTables[name].columns:
+                topTables[name].at[idx, (cat, subCol)] = _maybeBold(topTables[name].at[idx, (cat, subCol)], bold.at[idx, cat])
 
     for name, tops in topTables.items():
-        saveTable.joinpath(f'{name}_top_{TOP_N}.tex').open('w').write(tops.to_latex(sparsify=True, escape=False))
+        keep = [NAMES[f] for f in CORE_FEATURES]
+        rows = tops[keep].to_latex(sparsify=True, escape=False).split('\n')
+        table = '\n'.join(rows[:3] + rows[4:])  # Drops the second level of the multicolumn index
+        saveTable.joinpath(f'{name}_top_{TOP_N}.tex').open('w').write(table)
 
     # Kullback-Leibler Divergence
-    kl = pd.Series(index=tops.columns, dtype=float)
-    kl.name = 'KL Divergence'
-    for cat in tops.columns:
-        p = dict()
-        for name, cnt in counts.items():
-            indices = np.asarray([idx for idx, ctgry in categoryMap.items() if ctgry == cat])
-            matchingCounts = cnt[indices]
-            totalMatches = sum(matchingCounts)
-            p[name] = matchingCounts / totalMatches
-        keep = np.logical_and(p['before'] != 0, p['after'] != 0)
-        kl[cat] = discreteKullbackLeibler(p['before'][keep], p['after'][keep])
-
-    saveTable.joinpath('KullbackLeiblerDivergenceSentiment.tex').open('w').write(kl.to_latex(escape=True))
+    # kl = pd.Series(index=tops.columns, dtype=float)
+    # kl.name = 'KL Divergence'
+    # for cat in tops.columns:
+    #     p = dict()
+    #     for name, cnt in counts.items():
+    #         indices = np.asarray([idx for idx, ctgry in categoryMap.items() if ctgry == cat])
+    #         matchingCounts = cnt[indices]
+    #         totalMatches = sum(matchingCounts)
+    #         p[name] = matchingCounts / totalMatches
+    #     keep = np.logical_and(p['before'] != 0, p['after'] != 0)
+    #     kl[cat] = discreteKullbackLeibler(p['before'][keep], p['after'][keep])
+    #
+    # saveTable.joinpath('KullbackLeiblerDivergenceSentiment.tex').open('w').write(kl.to_latex(escape=True))
 
 
 def main():
@@ -133,7 +141,8 @@ def main():
 
     def _extract_pattern(p: str) -> str:
         """Takes an annotated and spark-ready regular expression and returns a human-readable version"""
-        return re.search(r'\\b(.*)\\b', p)[1]
+        pat = re.search(r'\\b(.*)\\b', p)[1]
+        return re.sub(r'\?', '', pat)
 
     idx2regex = {i: _extract_pattern(ptrn) for i, ptrn in data['mapping'].items()}
     idx2cat = {i: NAMES['_'.join(ptrn.split('_')[:2])] for i, ptrn in data['mapping'].items()}  # Human-readable naming
